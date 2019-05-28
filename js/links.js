6e8d5c892a0a9b0a440578437584a2fdae5a8332
mila-nova <mail@milakononova.com>
2019-05-28 19:47:46 +0200

    adds banners.html to load into banners ID

--- js/links.js
+++ js/links.js
@@ -3,4 +3,5 @@ $(function () {
     $("#header").load("header.html");
     $("#footer").load("footer.html");
     $("#fb-footer").load("fb-footer.html");
+    $("#banners").load("banners.html");
 });
