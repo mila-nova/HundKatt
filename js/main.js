a2c22411aebeb39474850fafbd9cf82f4f5eb1a8
mila-nova <mail@milakononova.com>
2019-06-01 17:10:18 +0200

    fixes function for back to top

--- js/main.js
+++ js/main.js
@@ -35,38 +35,33 @@ function pnummerValidate() {
 // do not run on small screens
 
 $(function () {
-    $("#service").load("service.html", function () {
-       
 
-        if ($(window).width() > 576) { 
+    if ($(window).width() > 576) { 
     
-            $(document).ready(function(){
-                $(window).scroll(function () {
-                       if ($(this).scrollTop() > 50) {
-                           $('#back-to-top').fadeIn();
-                       } else {
-                           $('#back-to-top').fadeOut();
-                       }
-                   });
-                   // scroll body to 0px on click
-                   $('#back-to-top').click(function () {
-                       $('#back-to-top').tooltip('hide');
-                       $('body,html').animate({
-                           scrollTop: 0
-                       }, 800);
-                       return false;
-                   });
-                   
-                   $('#back-to-top').tooltip('show');
-            
-            });
-            
-        }
+        $(document).ready(function(){
+            $(window).scroll(function () {
+                   if ($(this).scrollTop() > 50) {
+                       $('#back-to-top').fadeIn();
+                   } else {
+                       $('#back-to-top').fadeOut();
+                   }
+               });
+               // scroll body to 0px on click
+               $('#back-to-top').click(function () {
+                   $('#back-to-top').tooltip('hide');
+                   $('body,html').animate({
+                       scrollTop: 0
+                   }, 800);
+                   return false;
+               });
+               
+               $('#back-to-top').tooltip('show');
+        
+        });
+        
+    }
 
-
-    });
 });
-
 
 
 // dynamic copyright year
